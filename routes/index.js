28f0ed7fbd57be399666e279871e27a03d058aa0
Haley <haley.pelletier@students.olin.edu>
2015-03-06 16:02:50 -0500

    add procfile

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,7 @@
-//Import models
-var mongoose = require('mongoose');
-var Article = require('.././models/articleModel.js');
 var routes = {};
 
 routes.home = function(req,res) {
 	res.render('home');
 }
-
 
 module.exports = routes;
